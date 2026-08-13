5c1d03124e582d9d1aac1a54e77236cbf550f820
MatLuch <mateoluc36@gmail.com>
2026-08-13 12:03:52 -0400

    simplified some code

--- functionTests.py
+++ functionTests.py
@@ -1,3 +1,4 @@
 from functions.caucaltor import cauc
 from functions.weather import weathe
 print(weathe("what's the weather in toronto"))
+print(cauc("Whats 10 minus 20"))
--- functions/caucaltor.py
+++ functions/caucaltor.py
@@ -1,28 +1,21 @@
 def cauc(text):
-    if "plus" in text or "minus" in text:
-        nums = []
-        i = 0
-        while i < len(text):
-            if text[i].isdigit():
-                count = 0
-                hold = ""
-                while(i + count < len(text) and text[i + count].isdigit()):
-                    hold += text[i + count]
-                    count += 1
-                nums.append(hold)
-                i += count
-                count = 0
-            else: 
-                i += 1
-        end = 0
+    nums = []
+    hold = ""
 
-        if "plus" in text and len(nums) >= 1:
-            for i in range(len(nums)):
-                end += int(nums[i])
-        elif "minus" in text and len(nums) >= 1:
-            end += int(nums[0])
-            for i in range(len(nums)):
-                if i > 0:
-                    end -= int(nums[i])
+    for char in text:
+        if char.isdigit():
+            hold += char
+        elif hold: # checks if the last one is false and hold contains something
+            nums.append(int(hold))
+            hold = ""
 
-        return "The answer is: " + str(end)
+    if hold: 
+        nums.append(int(hold))
+
+    if "plus" in text:
+        answer = sum(nums)
+
+    elif "minus" in text:
+        answer = nums[0] - sum(nums[1:])
+
+    return "the answer is: " + str(answer)
