7e8da918e02323181b1535d96e6e1e9564085cfe
MatLuch <mateoluc36@gmail.com>
2026-08-22 14:03:29 -0400

    some small file changes

--- functionTests.py
+++ functionTests.py
@@ -2,4 +2,4 @@ from functions.caucaltor import cauc
 from functions.weather import tempature
 from functions.weather import rain
 from pointer import pointer
-print(rain("what's the temperature in Richmond Hill"))
+print(rain("what's the temperature in Toronto"))
